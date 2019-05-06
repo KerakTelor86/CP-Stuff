6e423c10596ad173685e496e5e71c1e68f7572ce
KerakTelor86 <43037910+KerakTelor86@users.noreply.github.com>
2019-05-07 00:05:35 +0700

    Update float_compare.cpp

--- Snippets/float_compare.cpp
+++ Snippets/float_compare.cpp
@@ -4,9 +4,9 @@ template<typename T>
 int fcmp(T a,T b,T eps)
 {
     T t=a-b,abst=t<0?-t:t;
-    if(abst<=eps) //if a == b
+    if(abst<eps) //if a == b
         return 0;
-    if(t>=-eps) //if a > b
+    if(t>-eps) //if a > b
         return 1;
     return -1; //if a < b
 }
