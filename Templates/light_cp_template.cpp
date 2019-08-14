f0eb200b4f7397eeee15864ec9f32cf34a119d27
Richard Alison <43037910+KerakTelor86@users.noreply.github.com>
2019-08-14 17:32:25 +0700

    Only main function, includes, and defines

---
+++ Templates/light_cp_template.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+#pragma GCC optimize("O2,unroll-loops")
+#pragma GCC target("avx,tune=native")
+
+#define fi first
+#define se second
+
+using namespace std;
+
+using ll=long long;
+using ld=long double;
+
+int main()
+{
+
+}
